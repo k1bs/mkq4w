f626f042ae812e8aa047460d11b3ce7d0c5cb3da
Alex Kibler <alexkibler@me.com>
2018-02-07 14:48:34 -0500

    toying around with pieces controller and routes

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,2 +1,25 @@
 class PiecesController < ApplicationController
+  def index
+    @pieces = Piece.order('created_at')
+  end
+
+  def new
+    @piece = Piece.new
+  end
+
+  def create
+    @piece = Piece.new(piece_params)
+    if @piece.save
+      flash[:success] = 'The piece was added!'
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def piece_params
+    params.require(:piece).permit(:image, :title, :year, :for_sale)
+  end
 end
